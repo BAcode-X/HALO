816fe6c2d204090ea022af6c17b138c029c4df68
wendirad <wendiradame@gmail.com>
2021-07-02 17:54:28 +0300

    [feat] Add User model and manager.py

--- src/auth.py
+++ src/auth.py
@@ -1,18 +1,56 @@
-import csv
+import os
+import uuid
+import json
+
+from hashlib import pbkdf2_hmac
+
+from exceptions import InvalidCreaditioal
 
 class User:
-    
+    primary_key = None
     username = None
     password = None
+    __fields = ['primary_key', 'username', 'password']
 
-    def hash_password(self, plaintext):
-        pass
+    def __init__(self, primary_key, username):
+        self.primary_key = primary_key
+        self.username = username
 
-    def register(self, username, password, *args, **kwargs):
-        pass
+    def __setattr__(self, attr, value, *args, **kwargs):
+        if attr not in self.__fields:
+            raise KeyError(f"Setting an attribute '{attr}'' is forbiden.")
+        return super().__setattr__(attr, value, *args, **kwargs)
 
-    def login(self, username, password):
-        pass
+    def __str__(self):
+        return self.primary_key.hex
 
-    def logout(self, user):
+class UserManager:
+    
+    @classmethod
+    def create_user(cls, username, password, new_password):
+        if password != new_password:
+            raise InvalidCreaditioal("Password mismatched")
+        if not username.isalnum():
+            raise TypeError("Username must be alphanumeric.")
+        user = User()
+        setattr(user, 'username', username)
+        setattr(user, 'password', cls.hash_password(password))
+
+        return user
+
+    @classmethod
+    def hash_password(cls, password):
+        hashed_password = pbkdf2_hmac('sha256', f'password'.encode(), os.urandom(10), 100000)
+        return hashed_password.hex()
+
+
+    @classmethod
+    def __filter_primary_key(cls, username):
+            with open('users.json', 'r') as f:
+                users = json.load(f)
+                users.get(username, None)
+
+
+    @classmethod
+    def authenticate(cls, username, password):
         pass
